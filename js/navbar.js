d1b7c2b265bad2a191a1fcec6fd5b758f9dc5fe2
Jansen Yan <jyan500@gmail.com>
2019-03-18 15:55:07 -0700

    added shadow to the project cards

--- js/navbar.js
+++ js/navbar.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 			let hash = this.hash;
 			$('html, body').animate({
 				scrollTop: $(hash).offset().top
-			}, 300, function(){
+			}, 500, function(){
 				// Add hash (#) to URL when done scrolling	
 				window.location.hash = hash;
 			});
