2cb7954d7c0037844e81f14cf0dc083bac341b95
Jansen Yan <jyan500@gmail.com>
2019-03-18 21:28:01 -0700

    removed the sticky header, and just replaced with normal header

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	// let header = $('header');
+
+	// // get offset position of the navbar 
+	// let sticky = $('header').offset().top;
+	// console.log('sticky offset position: ' , sticky);
+	// // add the sticky class to the header when you reach its scroll position
+	// $(window).scroll(function(){
+	// 	let scrollPosition = $(this).scrollTop();
+	// 	if (scrollPosition >= sticky){
+	// 		$('header').addClass("sticky");
+	// 	}
+	// 	else {
+	// 		$('header').removeClass("sticky");
+	// 	}
+	// });
+});
