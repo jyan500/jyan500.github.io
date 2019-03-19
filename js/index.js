b237e9a1082a71dcb0275d7d2fe9ae2c722fde7b
Jansen Yan <jyan500@gmail.com>
2019-03-18 23:11:42 -0700

    changed back to a sticky header, but with fade-in and fade-out, as well as color changing

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
 $(document).ready(function(){
 	// let header = $('header');
-
 	// // get offset position of the navbar 
-	// let sticky = $('header').offset().top;
-	// console.log('sticky offset position: ' , sticky);
-	// // add the sticky class to the header when you reach its scroll position
-	// $(window).scroll(function(){
-	// 	let scrollPosition = $(this).scrollTop();
-	// 	if (scrollPosition >= sticky){
-	// 		$('header').addClass("sticky");
-	// 	}
-	// 	else {
-	// 		$('header').removeClass("sticky");
-	// 	}
-	// });
+	let sticky = $('header').offset().top;
+	console.log('sticky offset position: ' , sticky);
+	// add the sticky class to the header when you reach its scroll position
+	$(window).scroll(function(){
+		let scrollPosition = $(this).scrollTop();
+		if (scrollPosition > sticky){
+			$('header').removeClass("fade-out");
+			$('header').addClass("sticky fade-in");
+			$('header').css('background', 'black');
+			$('header').css('color', 'white');
+			$('header a').css('color', 'white');
+		}
+		else {
+			$('header').removeClass("sticky fade-in");
+			$('header').addClass("fade-out");
+			$('header').css('background', 'white');
+			$('header').css('color', 'black');
+			$('header a').css('color', 'black');
+		}
+	});
 });
--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+
+	// default normal hover, with black text and black hover 
+	$('header ul li').addClass('normal-hover');
+	// smooth scroll
 	$("header a").click(function(e){
 		if (this.hash !== ""){
 			// prevent anchor default
@@ -10,6 +14,29 @@ $(document).ready(function(){
 				// Add hash (#) to URL when done scrolling	
 				window.location.hash = hash;
 			});
+		}
+	});
+
+	// handle sticky navbar 
+	// get offset position of the navbar 
+	let sticky = $('header').offset().top;
+	console.log('sticky offset position: ' , sticky);
+	// add the sticky class to the header when you reach its scroll position
+	$(window).scroll(function(){
+		let scrollPosition = $(this).scrollTop();
+		if (scrollPosition > sticky){
+			$('header ul li').removeClass('normal-hover');
+			$('header ul li').addClass('reverse-hover');
+			$('header').removeClass("fade-out");
+			$('header').addClass("sticky fade-in");
+			$('header').css('background', 'black');
+		}
+		else {
+			$('header').removeClass("sticky fade-in");
+			$('header ul li').removeClass('reverse-hover');
+			$('header').addClass("fade-out");
+			$('header ul li').addClass('normal-hover');
+			$('header').css('background', 'white');
 		}
 	});
 });
